a38e0fa564d5cb9bc7883018019ca31cb32524ac
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-09-02 16:04:23 -0500

    Update realTimeJobAlerts.py

--- realTimeJobAlerts.py
+++ realTimeJobAlerts.py
@@ -1,4 +1,6 @@
 import smtplib
+import win10toast
+from playsound import playsound
 import os
 import re
 import time
@@ -11,10 +13,10 @@ from bs4 import BeautifulSoup
 
 class myWorkday:
     def __init__(self):
-        self.url = 'https://REDACTED.com'
+        self.url = 'https://www.REDACTED.com/REDACTED/task/1422$2354.htmld'
         self.jobGroupToFind = '4'
         self.workDayUser = 'REDACTED@REDACTED.com'
-        self.workDayPass = os.environ.get('REDACTED_PASS')
+        self.workDayPass = os.environ.get('WORKDAY_PASS')
         self.emailUser = os.environ.get('EMAIL_USER')
         self.emailPass = os.environ.get('EMAIL_PASS')
         self.jobGroupRegEx = re.compile('JG\s(\d{1}\+?).*')
@@ -23,47 +25,50 @@ class myWorkday:
         self.browser = start_chrome(url=self.url, headless=True) # Create an instance of Google Chrome
 
         time.sleep(5)
-        write(self.workDayUser)                                  # Write username
+        write(self.workDayUser)                   # Write username
 
         time.sleep(1)  
-        press(ENTER)                                             # Press ENTER to move to the password screen
+        press(ENTER)                              # Press ENTER to move to the password screen
 
         time.sleep(2)               
-        write(self.workDayPass)                                  # Write password
+        write(self.workDayPass)                   # Write password
 
         time.sleep(1)              
-        press(ENTER)                                             # Press ENTER to move to the confirmation screen
+        press(ENTER)                              # Press ENTER to move to the confirmation screen
 
         time.sleep(3)
-        press(ENTER)                                             # Press ENTER to move to Workday's data
+        press(ENTER)                              # Press ENTER to move to Workday's data
 
-        time.sleep(5)                                            # Wait for the webpage to load
+        time.sleep(5)                             # Wait for the webpage to load
 
     def scrapeHTML(self):
         self.soup = BeautifulSoup(self.browser.page_source, 'html.parser')
 
-        self.myJSON = json.loads(self.soup.body.find('pre').next)
-
-        self.totalJobs = self.myJSON['body']['facetContainer']['paginationCount']['text']           # Total number of jobs
-
-        self.listItems = self.myJSON['body']['children'][0]['listItems']                            # The list items
-
         self.rows = []
 
-        for i in range(len(self.listItems)):
-            self.ID = self.listItems[i]['subtitles'][0]['value']                                    # Job ID
-            
-            self.title = self.listItems[i]['title']['instances'][0]['text']                         # Job Title
-            
-            self.postingDate = self.listItems[i]['subtitles'][1]['value'].split(': ')[1]            # Job Posting Date
-            
-            self.location = self.listItems[i]['subtitles'][2]['instances'][0]['text']               # Job Location
-            
-            self.jobURL = self.listItems[i]['title']['selfUriTemplate']
-            self.jobURL = self.jobURL[:4] + '/d' + self.jobURL[4:]
-            self.jobURL = 'https://www.REDACTED.com' + self.jobURL + '.htmld'                       # Job URL
-            
-            self.rows.append((self.ID, self.title, self.postingDate, self.location, self.jobURL))   # Append data to list
+        try:
+            self.myJSON = json.loads(self.soup.body.find('pre').next)                                   # In rare cases, will give an AttributeError: 'NoneType' object has no attribute 'next'
+
+            self.totalJobs = self.myJSON['body']['facetContainer']['paginationCount']['text']           # Total number of jobs
+
+            self.listItems = self.myJSON['body']['children'][0]['listItems']                            # The list items
+
+            for i in range(len(self.listItems)):
+                self.ID = self.listItems[i]['subtitles'][0]['value']                                    # Job ID
+                
+                self.title = self.listItems[i]['title']['instances'][0]['text']                         # Job Title
+                
+                self.postingDate = self.listItems[i]['subtitles'][1]['value'].split(': ')[1]            # Job Posting Date
+                
+                self.location = self.listItems[i]['subtitles'][2]['instances'][0]['text']               # Job Location
+                
+                self.jobURL = self.listItems[i]['title']['selfUriTemplate']
+                self.jobURL = self.jobURL[:4] + '/d' + self.jobURL[4:]
+                self.jobURL = 'https://www.REDACTED.com' + self.jobURL + '.htmld'                      # Job URL
+                
+                self.rows.append((self.ID, self.title, self.postingDate, self.location, self.jobURL))   # Append data to list
+        except Exception as e:
+            pass
 
         kill_browser()
 
@@ -108,6 +113,7 @@ class myWorkday:
         self.validTitles = []
         self.validJobs = []
         self.validLocations = []
+        self.validHiringManagers = []
 
         if len(self.jobURLS) == 0:
             pass
@@ -146,7 +152,15 @@ class myWorkday:
                 else:
                     pass
 
-                self.jobGroup = re.search(self.jobGroupRegEx, jg).group(1)
+                try:
+                    self.jobGroup = re.search(self.jobGroupRegEx, jg).group(1)
+                except Exception as e:
+                    self.jobGroup = 'Can\'t find the JG in the HTML'
+
+                try:
+                    self.hiringManager = self.soup.find(id='promptOption-gwt-uid-14').text
+                except Exception as e:
+                    self.hiringManager = 'Could not find the hiring manager in the HTML.'
 
                 try:
                     self.location = self.soup.find(id='promptOption-gwt-uid-8').text
@@ -159,6 +173,7 @@ class myWorkday:
                     self.validTitles.append(self.jobTitles[i])
                     self.validLocations.append(self.location)
                     self.validJobs.append(self.jobURLS[i])
+                    self.validHiringManagers.append(self.hiringManager)
                 else:
                     pass
                 
@@ -166,42 +181,55 @@ class myWorkday:
             
             print(f'\nThere are {len(self.validJobs)} jobs matching your job group criterion.')
 
-    def sendTheMail(self):
+    def myToaster(self):
         if len(self.validJobs) == 0:
             # Do nothing
             pass
         else:
-            # Send the email
-            with smtplib.SMTP_SSL(host='smtp.gmail.com', port=465) as smtp:
-                for i in range(len(self.validJobs)):
-                    # Build the email message
-                    self.msg = EmailMessage()
-                    self.msg['Subject'] = 'A New Job Has Been Found!'
-                    self.msg['From'] = self.emailUser
-                    self.msg['To'] = self.workDayUser
-                    self.msg.set_content(f"""\
-                    The job title: {self.validTitles[i]}\n
-                    The job location: {self.validLocations[i]}\n
-                    The job URL: {self.validJobs[i]}\n
-                    ***This message is sent from Python***
-                    """)
-                    self.msg.add_alternative(f"""\
-                    <!DOCTYPE html>
-                    <html>
-                        <body>
-                            <h1 style="color:red;"> Title: {self.validTitles[i]}</h1>
-                            <h1 style="color:blue;"> Location: {self.validLocations[i]}</h1>
-                            <h2 style="color:green;"> URL: {self.validJobs[i]}</h2>
-                            <p style="color:Black;"> This message is sent from Python</p>
-                            <img src="C:\\Users\\REDACTED\\Desktop\\Documents\\Miscellaneous\\python-logo-master-v3-TM.png" alt="Python Programming" width="601" height="203">
-                        </body>
-                    </html>
-                    """, subtype='html')
+            for i in range(len(self.validJobs)):
+                self.toaster = win10toast.ToastNotifier()
 
-                    # Log in and send the message
-                    smtp.login(user=self.emailUser, password=self.emailPass)
-                    smtp.send_message(msg=self.msg)
-                    print(f'\nSent an email at {time.strftime("%I:%M:%S")}')
+                # playsound(sound='newJob.mp3')
+                # time.sleep(3)
+                self.toaster.show_toast(title='A New Job Has Been Posted!', msg=f'Title: {self.validTitles[i]}\nLocation: {self.validLocations[i]}\nJob Group: {self.jobGroupToFind}\nHiring Manager: {self.validHiringManagers[i]}', icon_path=r"C:\Users\REDACTED\Desktop\Documents\Miscellaneous\work.ico", duration=25)
+
+    # def sendTheMail(self):
+    #     if len(self.validJobs) == 0:
+    #         # Do nothing
+    #         pass
+    #     else:
+    #         # Send the email
+    #         with smtplib.SMTP_SSL(host='smtp.gmail.com', port=465) as smtp:
+    #             for i in range(len(self.validJobs)):
+    #                 time.sleep(5)
+    #                 # Build the email message
+    #                 self.msg = EmailMessage()
+    #                 self.msg['Subject'] = 'A New Job Has Been Found!'
+    #                 self.msg['From'] = self.emailUser
+    #                 self.msg['To'] = self.workDayUser
+    #                 self.msg.set_content(f"""\
+    #                 The job title: {self.validTitles[i]}\n
+    #                 The job location: {self.validLocations[i]}\n
+    #                 The job URL: {self.validJobs[i]}\n
+    #                 ***This message is sent from Python***
+    #                 """)
+    #                 self.msg.add_alternative(f"""\
+    #                 <!DOCTYPE html>
+    #                 <html>
+    #                     <body>
+    #                         <h1 style="color:red;"> Title: {self.validTitles[i]}</h1>
+    #                         <h1 style="color:blue;"> Location: {self.validLocations[i]}</h1>
+    #                         <h2 style="color:green;"> URL: {self.validJobs[i]}</h2>
+    #                         <p style="color:Black;"> This message is sent from Python</p>
+    #                         <img src="C:\\Users\\REDACTED\\Desktop\\Documents\\Miscellaneous\\python-logo-master-v3-TM.png" alt="Python Programming" width="601" height="203">
+    #                     </body>
+    #                 </html>
+    #                 """, subtype='html')
+
+    #                 # Log in and send the message
+    #                 smtp.login(user=self.emailUser, password=self.emailPass)
+    #                 smtp.send_message(msg=self.msg)
+    #                 print(f'\nSent an email at {time.strftime("%I:%M:%S")}')
 
 # Create a variable that will keep track of how many runs are done in a session
 runs = 1
@@ -212,8 +240,9 @@ while True:
     WORKDAY = myWorkday()
     WORKDAY.startBrowser()
     WORKDAY.scrapeHTML()
-    WORKDAY.toDatabase(r'C:\Users\REDACTED\myIPYNBs\REDACTED.db')
+    WORKDAY.toDatabase(r'C:\Users\REDACTED\myIPYNBs\Workday.db')
     WORKDAY.findOurJobs()
-    WORKDAY.sendTheMail() 
+    WORKDAY.myToaster()
+    #WORKDAY.sendTheMail() 
     runs += 1            # Increment the run count by 1
-    time.sleep(200)      # Amount of time (in seconds) to wait between runs
+    time.sleep(600)      # Amount of time (in seconds) to wait between runs
